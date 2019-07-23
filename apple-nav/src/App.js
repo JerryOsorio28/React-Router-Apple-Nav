1278fa230a6a6573a9b12bac5fa571d039dfabef
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-07-23 15:17:51 -0400

    Added styling to navbars and added icon images

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import ParentNavBar from './components/ParentNavBar';
 import NavBar from './components/NavBar';
 
+import './styles/styles.css';
+
 function App() {
   return (
     <div className="App">
